c0b92653a6fb9e71ee37d087be8544d058369f9a
曲宇杰 <ququ1120@126.com>
2017-08-16 16:56:22 +0800

    heart beat

---
+++ src/main/java/fourthsample/MyServerInitializer.java
@@ -0,0 +1,24 @@
+package fourthsample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * Created by ququ1 on 2017/8/16.
+ */
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new IdleStateHandler(5, 7, 10, TimeUnit.SECONDS));
+        pipeline.addLast(new MyServerHandler());
+    }
+}
