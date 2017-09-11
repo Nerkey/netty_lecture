74add6e9416fa7448af85d4dd51ebdd9548e5b83
曲宇杰 <ququ1120@126.com>
2017-09-11 15:05:22 +0800

    io切换为nio

--- src/main/java/com/shengsiyuan/nio/NioTest2.java
+++ src/main/java/com/shengsiyuan/nio/NioTest2.java
@@ -1,7 +1,7 @@
 package com.shengsiyuan.nio;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
 /**
@@ -9,11 +9,22 @@ import java.nio.channels.FileChannel;
  */
 public class NioTest2 {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws Exception {
 
         FileInputStream fileInputStream = new FileInputStream("NioTest2.txt");
         FileChannel fileChannel = fileInputStream.getChannel();
 
+        ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+        fileChannel.read(byteBuffer);
+
+        byteBuffer.flip();
+
+        while (byteBuffer.remaining() > 0) {
+            byte b = byteBuffer.get();
+            System.out.println("Character: " + (char)b);
+        }
+
+        fileInputStream.close();
 
     }
 
