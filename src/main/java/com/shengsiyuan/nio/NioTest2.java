30caf18b4148870e379c7e84a857312d91a9528d
曲宇杰 <ququ1120@126.com>
2017-09-11 14:57:51 +0800

    生成随机数, 打印

--- src/main/java/com/shengsiyuan/nio/NioTest2.java
+++ src/main/java/com/shengsiyuan/nio/NioTest2.java
@@ -2,6 +2,7 @@ package com.shengsiyuan.nio;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.nio.channels.FileChannel;
 
 /**
  * io切换nio
@@ -11,7 +12,7 @@ public class NioTest2 {
     public static void main(String[] args) throws FileNotFoundException {
 
         FileInputStream fileInputStream = new FileInputStream("NioTest2.txt");
-
+        FileChannel fileChannel = fileInputStream.getChannel();
 
 
     }
