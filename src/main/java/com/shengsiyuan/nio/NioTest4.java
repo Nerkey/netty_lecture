1d186ba612e0be64ea86022e64dab1a273eeb811
曲宇杰 <ququ1120@126.com>
2017-09-12 17:11:26 +0800

    从一个文件中获取文件内容, 然后写入到另外一个文件中

---
+++ src/main/java/com/shengsiyuan/nio/NioTest4.java
@@ -0,0 +1,44 @@
+package com.shengsiyuan.nio;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * 从一个文件中获取文件内容, 然后写入到另外一个文件中
+ */
+public class NioTest4 {
+
+    public static void main(String[] args) throws Exception {
+        FileInputStream inputStream = new FileInputStream("input.txt");
+        FileOutputStream outputStream = new FileOutputStream("output.txt");
+
+        FileChannel inputChannel = inputStream.getChannel();
+        FileChannel outputChannel = outputStream.getChannel();
+
+        ByteBuffer buffer = ByteBuffer.allocate(512);
+
+        while (true) {
+//            buffer.clear();
+
+            int read = inputChannel.read(buffer);
+
+            System.out.println("read: " + read);
+
+            if (-1 == read) {
+                break;
+            }
+
+            buffer.flip();
+
+            outputChannel.write(buffer);
+        }
+
+        inputStream.close();
+        outputStream.close();
+    }
+
+
+}
