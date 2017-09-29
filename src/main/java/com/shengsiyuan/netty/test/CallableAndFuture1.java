6d97cf8e810831e35ac53f198c3c060290e2635b
曲宇杰 <ququ1120@126.com>
2017-09-29 17:34:15 +0800

    'callableandfuture

---
+++ src/main/java/com/shengsiyuan/netty/test/CallableAndFuture1.java
@@ -0,0 +1,26 @@
+package com.shengsiyuan.netty.test;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+public class CallableAndFuture1 {
+    public static void main(String[] args) {
+        Callable<Integer> callable = new Callable<Integer>() {
+            public Integer call() throws Exception {
+                return new Random().nextInt(100);
+            }
+        };
+        FutureTask<Integer> future = new FutureTask<Integer>(callable);
+        new Thread(future).start();
+        try {
+            Thread.sleep(5000);// 可能做一些事情
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/shengsiyuan/netty/test/CallableAndFuture2.java
@@ -0,0 +1,23 @@
+package com.shengsiyuan.netty.test;
+
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class CallableAndFuture2 {
+    public static void main(String[] args) {
+        ExecutorService threadPool = Executors.newSingleThreadExecutor();
+        Future<Integer> future = threadPool.submit(new Callable<Integer>() {
+            public Integer call() throws Exception {
+                return new Random().nextInt(100);
+            }
+        });
+        try {
+            Thread.sleep(5000);// 可能做一些事情
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
